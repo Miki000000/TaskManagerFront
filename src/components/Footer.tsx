da347d9f13c94a1e250acb5a00120fcc0a21d462
Vitor <vitorhugosnp@gmail.com>
2025-03-06 17:03:00 -0400

    fix: Fixed footer position at the bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer class="footer sm:footer-horizontal text-neutral-content items-center p-4 flex justify-between">
+      <footer class="footer sm:footer-horizontal text-neutral-content items-center p-4 flex justify-between fixed bottom-0 w-full">
         <aside class="flex justify-between">
           <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
         </aside>
