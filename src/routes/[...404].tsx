9d5d1886f3b6125c529a1e0141330196e06d3a4c
Vitor <vitorhugosnp@gmail.com>
2025-03-11 08:26:47 -0400

    Minor changes

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -5,9 +5,11 @@ export default function NotFound() {
     <main class="min-h-screen hero bg-base-200">
       <div class="hero-content text-center">
         <div class="max-w-md">
-          <h1 class="text-6xl font-bold text-primary mb-8">404</h1>
-          <h2 class="text-3xl font-semibold mb-4">Página não encontrada</h2>
-          <p class="mb-8">
+          <h1 class="text-6xl font-bold mb-8 text-primary">404</h1>
+          <h2 class="text-3xl font-semibold mb-4 text-base-content">
+            Página não encontrada
+          </h2>
+          <p class="mb-8 text-base-content">
             Desculpe, a página que você está procurando não existe ou foi
             movida.
           </p>
