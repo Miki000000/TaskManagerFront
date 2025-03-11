318b36ed6d5ec11e362a0ecf6ba1c2edcea3df02
Vitor <vitorhugosnp@gmail.com>
2025-03-11 08:24:32 -0400

    Minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer class="footer sm:footer-horizontal text-neutral-content items-center p-4 flex justify-between fixed bottom-0 w-full">
+      <footer class="footer sm:footer-horizontal items-center p-4 flex justify-between fixed bottom-0 w-full bg-base-300 text-base-content">
         <aside class="flex justify-between">
           <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
         </aside>
